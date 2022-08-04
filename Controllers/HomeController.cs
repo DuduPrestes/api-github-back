8989575f678cbc468d263c3bfd97cea73bbc0bf5
DuduPrestes <edu.pro.contato@gmail.com>
2022-08-03 22:13:22 -0300

    First

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("GetUsuario")]
+        public ActionResult GetUsuario(string usuario)
+        {
+            try
+            {
+                string url = "https://api.github.com/users/" + usuario;
+
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.UserAgent = "request";
+
+                HttpWebResponse res;
+
+                try
+                {
+                    res = (HttpWebResponse)req.GetResponse();
+                }
+                catch (Exception)
+                {
+                    return new ContentResult() { StatusCode = (int)HttpStatusCode.NoContent };
+                }
+
+                string user = "";
+
+                using (Stream stream = res.GetResponseStream())
+                {
+                    StreamReader sr = new StreamReader(stream);
+                    user = sr.ReadToEnd();
+                }
+
+                string repos = "";
+
+                url = "https://api.github.com/users/" + usuario + "/repos";
+                req = (HttpWebRequest)WebRequest.Create(url);
+                req.UserAgent = "request";
+                res = (HttpWebResponse)req.GetResponse();
+
+                using (Stream stream = res.GetResponseStream())
+                {
+                    StreamReader sr = new StreamReader(stream);
+                    repos = sr.ReadToEnd();
+                }
+
+                string content = "[" + user + "," + repos + "]";
+
+                return new ContentResult() { Content = content, StatusCode = (int)HttpStatusCode.OK };
+            }
+            catch (Exception ex)
+            {
+                return new ContentResult() { Content = ex.Message, StatusCode = (int)HttpStatusCode.InternalServerError };
+            }
+        }
+    }
+}
